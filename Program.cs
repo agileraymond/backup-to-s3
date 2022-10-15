5b6cf408bfcf0faae5c2d68eb2677eb3a25e464b
Raymond Sanchez <agileRaymond@gmail.com>
2022-10-15 15:57:44 -0500

    add s3 nuget package

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@
 
 // if I file fails to sync, write to a log file 
 
+using Amazon.S3;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -17,15 +18,37 @@ IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
 // read config value with localFolder > aws s3 bucket
 var folderMappings = config.GetValue<string>("FolderMappings");
 
-foreach (var folderConfig in folderMappings.Split(';'))
-{
-    var folderArray = folderConfig.Split(':');
-    Console.WriteLine($"local folder: {folderArray[0]} s3: {folderArray[1]}");
-    //Directory.SetCurrentDirectory(folderArray[0]);
+// s3 client 
+var s3Client = new AmazonS3Client();
 
-    var fileEntries = new DirectoryInfo(Path.Combine(".", folderArray[0])).GetFiles("*", SearchOption.AllDirectories);
+foreach (var folderConfig in folderMappings.Split(';'))
+{   
+    var folderArray = folderConfig.Split(':');     
+    var folder = Path.Join("c:\\", folderArray[0]);
+    var s3Bucket = folderArray[1];
+    Console.WriteLine(folder);
+    var fileEntries = new DirectoryInfo($"{folder}").GetFiles("*", SearchOption.AllDirectories);
+    
     foreach (var file in fileEntries)
     {
         Console.WriteLine($"file {file}");    
+        DateTime dt = File.GetLastAccessTime(file.ToString());
+        Console.WriteLine("The last access time for this file was {0}.", dt);   
+
+        // upload to s3
+        try
+        {
+            await s3Client.PutObjectAsync(new Amazon.S3.Model.PutObjectRequest
+            { 
+                BucketName = s3Bucket,
+                Key = file.Name,
+                FilePath = file.ToString(),
+                ContentType = "text/plain"
+            });
+        }
+        catch (System.Exception ex)
+        {
+            Console.WriteLine(ex.Message);    
+        } 
     }
 }
