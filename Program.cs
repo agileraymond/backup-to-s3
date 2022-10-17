025e543626216d387b0d6ca8f1ce31c1b94f343d
Raymond Sanchez <agileRaymond@gmail.com>
2022-10-17 13:07:40 -0500

    final changes

--- Program.cs
+++ Program.cs
@@ -1,13 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-
-// loop thru localFolders and get files 
-
-// check last modified date on file and sync file that meet a config date
-
-// if I file fails to sync, write to a log file 
-
-using Amazon.S3;
+﻿using Amazon.S3;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -17,38 +8,42 @@ IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
 
 // read config value with localFolder > aws s3 bucket
 var folderMappings = config.GetValue<string>("FolderMappings");
+var rootDrive = "c:\\";
+var lastModifiedMinutes = config.GetValue<double>("SyncFilesLastModifiedMinutes");
 
 // s3 client 
 var s3Client = new AmazonS3Client();
+var checkDate = DateTime.Now.AddMinutes(-lastModifiedMinutes);
 
 foreach (var folderConfig in folderMappings.Split(';'))
 {   
     var folderArray = folderConfig.Split(':');     
-    var folder = Path.Join("c:\\", folderArray[0]);
+    var folder = Path.Join(rootDrive, folderArray[0]);
     var s3Bucket = folderArray[1];
     Console.WriteLine(folder);
     var fileEntries = new DirectoryInfo($"{folder}").GetFiles("*", SearchOption.AllDirectories);
     
     foreach (var file in fileEntries)
     {
-        Console.WriteLine($"file {file}");    
-        DateTime dt = File.GetLastAccessTime(file.ToString());
-        Console.WriteLine("The last access time for this file was {0}.", dt);   
+        if (file.LastWriteTime >= checkDate)
+        {
+            var key = file.FullName.Replace(rootDrive, "").Replace("\\", "/");
 
-        // upload to s3
-        try
-        {
-            await s3Client.PutObjectAsync(new Amazon.S3.Model.PutObjectRequest
-            { 
-                BucketName = s3Bucket,
-                Key = file.Name,
-                FilePath = file.ToString(),
-                ContentType = "text/plain"
-            });
+            // upload to s3
+            try
+            {
+                await s3Client.PutObjectAsync(new Amazon.S3.Model.PutObjectRequest
+                {
+                    BucketName = s3Bucket,
+                    Key = key,
+                    FilePath = file.ToString(),
+                    ContentType = "text/plain"
+                });
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
-        catch (System.Exception ex)
-        {
-            Console.WriteLine(ex.Message);    
-        } 
     }
 }
